d3071f349d7112ff73241cf62db45bfba2b36079
lixun911 <mydesktop911@gmail.com>
2013-08-14 16:21:29 +0300

    Create prepare-XBMC.sh

---
+++ prepare-XBMC.sh
@@ -0,0 +1,7 @@
+echo "####### Prepare the XBMC code for compilation ########"
+
+cd xbmc-rbp/
+    sed -i 's/USE_BUILDROOT=1/USE_BUILDROOT=0/' tools/rbp/setup-sdk.sh
+    sed -i 's/TOOLCHAIN=\/usr\/local\/bcm-gcc/TOOLCHAIN=\/usr/' tools/rbp/setup-sdk.sh
+    sudo sh tools/rbp/setup-sdk.sh
+    sed -i 's/cd $(SOURCE); $(CONFIGURE)/#cd $(SOURCE); $(CONFIGURE)/' tools/rbp/depends/xbmc/Makefile
