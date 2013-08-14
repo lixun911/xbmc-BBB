b365409f93b746a575d4acc652519c19b7e8f5c3
lixun911 <mydesktop911@gmail.com>
2013-08-14 16:21:50 +0300

    Update git-XBMC.sh

--- git-XBMC.sh
+++ git-XBMC.sh
@@ -3,10 +3,3 @@ echo "####### Get the latest XBMC from git ######"
 sudo git clone --depth 1 git://github.com/xbmc/xbmc-rbp.git
 
 
-echo "####### Prepare the XBMC code for compilation ########"
-
-cd xbmc-rbp/
-    sed -i 's/USE_BUILDROOT=1/USE_BUILDROOT=0/' tools/rbp/setup-sdk.sh
-    sed -i 's/TOOLCHAIN=\/usr\/local\/bcm-gcc/TOOLCHAIN=\/usr/' tools/rbp/setup-sdk.sh
-    sudo sh tools/rbp/setup-sdk.sh
-    sed -i 's/cd $(SOURCE); $(CONFIGURE)/#cd $(SOURCE); $(CONFIGURE)/' tools/rbp/depends/xbmc/Makefile
